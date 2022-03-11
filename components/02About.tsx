ca4bdb406b9911f9b80bcaa074649f0f519349b0
t4t5u0 <ymmtryk0902@gmail.com>
2022-03-11 16:26:41 +0900

    Update: 02 css

--- components/02About.tsx
+++ components/02About.tsx
@@ -3,22 +3,26 @@ import Image from "next/image";
 const About = (): JSX.Element => {
   return (
     <>
-      <div className="area">
-        <Image
-          src={
-            "https://images.pexels.com/photos/56857/animal-cat-kitten-funny-56857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          }
-          width={800}
-          height={600}
-          alt="yawning"
-        />
-        <h3>あくびってうつりませんか？</h3>
-        <p>
-          友達・家族につられて あくびをしてしまった。 大事な会議中。
-          上司にあくびをうつしてしまった。
-          自分の意思に反してうつり・うつってしまう「あくび」
-          Yawningはあくびを繋ぐことで人にうつす体験ができる
-          新時代のあくびネットワークです。
+      <div className={`area container`} >
+        <div className="item1">
+          <Image
+            src={
+              "https://images.pexels.com/photos/56857/animal-cat-kitten-funny-56857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            }
+            width={828 * 0.6}
+            height={552 * 0.6}
+            alt="yawning"
+          />
+        </div>
+        <h3 className={`subTitle item2`}>あくびってうつりませんか？</h3>
+        <p className="item3">
+          友達・家族につられて <br />
+          あくびをしてしまった。 <br /><br />
+          大事な会議中。<br />
+          上司にあくびをうつしてしまった。<br /><br />
+          自分の意思に反してうつり・うつってしまう「あくび」 <br />
+          Yawningはあくびを繋ぐことで人にうつす体験ができる <br />
+          新時代のあくびネットワークです。<br />
         </p>
       </div>
 
@@ -27,6 +31,43 @@ const About = (): JSX.Element => {
           scroll-snap-align: start;
           height: 100vh;
         }
+
+        .subTitle{
+          text-align: left;
+          font: normal normal normal 48px/82px Hiragino Sans;
+          color: #697BFF;
+        }
+
+        .container{
+          display: grid;
+          grid-template-columns: 50vw 50vw;
+          grid-template-rows: 200px auto;
+          
+          padding-top: 100px;
+        }
+
+        .item1{
+          grid-row: 1/3;
+          grid-column: 1;
+          align-self: center;
+          justify-self: center;
+        }
+
+        .item2{
+          grid-row: 1/2;
+          grid-column: 2;
+          justify-self: left;
+        }
+
+        .item3{
+          grid-row: 2/3;
+          grid-column: 2;
+          
+
+          font: normal normal normal 24px/40px Hiragino Sans;
+          color: #4D4D4D;
+        }
+
       `}</style>
     </>
   );
