be2811329f00136b036e54c3b16430ab036847a1
t4t5u0 <ymmtryk0902@gmail.com>
2022-03-11 16:43:35 +0900

    03 css

--- components/03Promotion.tsx
+++ components/03Promotion.tsx
@@ -4,14 +4,34 @@ const Promotion = () => {
   return (
     <>
       <div className="area">
-        <h2>起こそう、あくびのニューウェーブ きっとできる。あなたのあくびで</h2>
-        <YouTube videoId="c5rjeSuXtrE"></YouTube>
+        <h2 className="subTitle">
+          起こそう、あくびのニューウェーブ <br />
+          きっとできる。あなたのあくびで</h2>
+        <div className="youtube">
+          <YouTube videoId="c5rjeSuXtrE" />
+        </div>
       </div>
       <style jsx>{`
         .area {
           scroll-snap-align: start;
           height: 100vh;
         }
+
+        .subTitle {
+          text-align: center;
+          font: normal normal normal 56px/80px Hiragino Sans;
+          color: #697BFF;
+          padding-top: 150px;
+        }
+
+        .youtube{
+          text-align: center;
+        }
+
+        iframe {
+          text-align: center;
+        }
+
       `}</style>
     </>
   );
